65d9805c9e9f91a550e6cc1098a0712e4345a37d
Kerem ILHAN <kerem7095@gmail.com>
2022-07-02 08:22:53 +0300

    Changes for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import AppRouter from './components/router/AppRouter';
 import { Toaster } from "react-hot-toast";
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './firebase';
 
 
 function App() {
+  initializeApp(firebaseConfig)
   return (
     <div>
       <Toaster />
